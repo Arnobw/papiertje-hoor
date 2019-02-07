4e8a062d15dfdbce584d08cd9b4b5e64285bd9b7
Arnobw <arnobw@hotmail.com>
2019-02-07 12:11:10 +0100

    tank

--- rock.js
+++ rock.js
@@ -3,63 +3,148 @@ console.log("\\____()< awooooo");
 let a = ["schaar", "steen", "papier"];
 let computerwin = 0;
 let menswin = 0;
-let prefix = "De computer koos "
+let prefix = "<De computer koos ";
+let ttank = 1;
 
 
 
 
 
 
-$('#btn').click(function(){
+$('#btn').click(function () {
     b = $('#input').val();
-   
+
     //winnen, verliezen, gelijkspel....
-    let win = function() {
-        $('#output').text(prefix + a[r] + ". Je hebt gewonnen, episch!");
-        $('#foto').css('background-image', "url('win.jpg')");
-        menswin +=1; 
-        $('#mens').text("jouw score: " + menswin);
+    let win = function () {
+        $('#output').animate({
+            opacity: 0
+        }, 50, function () {
+            $(this).text(prefix + a[r] + ". Je hebt gewonnen! Episch!>")
+                .animate({
+                    opacity: 1
+                });
+        })
+        $('#foto').animate({
+            opacity: 0
+        }, 50, function () {
+            $(this).css('background-image', "url('win.jpg')")
+                .animate({
+                    opacity: 1
+                });
+        })
+
+
+        menswin += 1;
+        $('#mens').text("jouw score: " + menswin)
     }
 
-    let draw = function(){
-        $('#output').text(prefix + a[r] +  ". Jullie staan gelijk.");
-        $('#foto').css('background-image', "url('draw.jpg')");
+    let draw = function () {
+        $('#output').animate({
+            opacity: 0
+        }, 50, function () {
+            $(this).text(prefix + a[r] + ". Jullie staan gelijk.>")
+                .animate({
+                    opacity: 1
+                });
+        })
+        $('#foto').animate({
+            opacity: 0
+        }, 50, function () {
+            $(this).css('background-image', "url('draw.jpg')")
+                .animate({
+                    opacity: 1
+                });
+        })
     }
 
-    let loss = function(){
-        $('#output').text(prefix + a[r] + ". Je verliest."); 
-        $('#foto').css('background-image', "url('lose.jpg')");
-        computerwin +=1;
-        $('#pc').text( "pc score :" + computerwin);
+    let loss = function () {
+        $('#output').animate({
+            opacity: 0
+        }, 50, function () {
+            $(this).text(prefix + a[r] + ". Je verliest.>")
+                .animate({
+                    opacity: 1
+                });
+        })
+        $('#foto').animate({
+            opacity: 0
+        }, 50, function () {
+            $(this).css('background-image', "url('lose.jpg')")
+                .animate({
+                    opacity: 1
+                });
+        })
+        computerwin += 1;
+        $('#pc').text("pc score :" + computerwin);
     }
-    let r = Math.floor(Math.random()*3);
- 
+
+
+
+    let r = Math.floor(Math.random() * 3);
+
+
     if (b === "schaar") {
         if (a[r] === "schaar") {
-           draw();
-            
+            draw();
+
         } else if (a[r] === "steen") {
-        loss();
+            loss();
         } else if (a[r] === "papier") {
             win();
         }
-    } else if (b ==="steen") {
+    } else if (b === "steen") {
         if (a[r] === "schaar") {
-           win();
+            win();
         } else if (a[r] === "steen") {
-           draw();
-        } else if (a[r] === "papier"){
-           loss();
+            draw();
+        } else if (a[r] === "papier") {
+            loss();
         }
     } else if (b === "papier") {
-        if(a[r] === "schaar") {
-           loss();
+        if (a[r] === "schaar") {
+            loss();
         } else if (a[r] === "steen") {
-          win();
+            win();
         } else if (a[r] === "papier") {
-           draw();
+            draw();
         }
     }
 
 
 });
+
+
+
+// tank
+let tank = function () {
+    $('#output').animate({
+        opacity: 0
+    }, 50, function () {
+        $(this).text("Tank verslaat alles.")
+            .animate({
+                opacity: 1
+            });
+    })
+
+    $('#foto').animate({
+        opacity: 0
+    }, 50, function () {
+        $(this).css('background-image', "url('tank.jpg')")
+            .animate({
+                opacity: 1
+            });
+    })
+    menswin -= ttank;
+    computerwin -= ttank;
+
+    $('#mens').text("jouw score: " + menswin);
+    $('#pc').text("pc score: " + computerwin);
+    ttank += 1;
+}
+
+$('#btn').click(function () {
+    let rr = Math.floor(Math.random() * 10);
+    if (rr == 5) {
+        tank();
+    }
+})
